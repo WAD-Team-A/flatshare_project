430bbf95ee5114605c9405a1cdd55338517fb51e
Niovi Lampiri <2404353l@student.gla.ac.uk>
2020-03-19 02:54:42 +0000

    updated UserProfile and Flat models

--- flatshare/admin.py
+++ flatshare/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from flatshare.models import Flat, Profile
+from flatshare.models import Flat, UserProfile
 
 admin.site.register(Flat)
-admin.site.register(Profile)
+admin.site.register(UserProfile)
 
 # Register your models here.
--- flatshare/models.py
+++ flatshare/models.py
@@ -1,33 +1,46 @@
+import uuid
+from django.utils.timezone import now
 from django.db import models
 from django.contrib.auth.models import User
 
 
-
-
-
-
-class Profile(models.Model):
+class UserProfile(models.Model):
     NAME_MAX_CHAR = 128
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    FirstName = models.CharField(max_length = NAME_MAX_CHAR, unique = False)
-    LastName = models.CharField(max_length= NAME_MAX_CHAR, unique = False)
+    FirstName = models.CharField(max_length=NAME_MAX_CHAR, unique=False)
+    LastName = models.CharField(max_length=NAME_MAX_CHAR, unique=False)
     picture = models.ImageField(upload_to='profile_images', blank=True)
-    phone_no = models.IntegerField(default = 888, unique = True)
-    age = models.IntegerField(default = 18)
+    phone_no = models.IntegerField(default=888, unique=True)
+    age = models.IntegerField(default=18)
+   #liked_flats = models.ManyToManyField(Flat)
+
     def __str__(self):
-        return (self.FirstName + " "+ self.LastName)
+        return self.FirstName + " " + self.LastName
+
+
+class Address(models.Model):
+    flat_no = models.CharField(max_length=5)
+    house_no = models.IntegerField()
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    province = models.CharField(max_length=100)
+    postcode = models.CharField(max_length=10)
+    country = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.flat_no + ' ' + self.house_no + ' ' + self.street + ' ' + self.city + ' ' + self.province + ' ' \
+               + self.postcode + ' ' + self.country
+
 
 class Flat(models.Model):
-    NAME_MAX_CHAR = 128
-    flat_id = models.IntegerField(default = 1, primary_key=True)
-    rent = models.IntegerField(default = 1)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    street = models.CharField(max_length = NAME_MAX_CHAR, unique = False)
-    house_no = models.CharField(max_length = 50, unique = False)
-    postCode = models.CharField(max_length = 50, unique = False)
-    city = models.CharField(max_length = 128, unique = False)
+    name = models.CharField(max_length=30)
+    flat_id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4())
+    address = Address
+    rent = models.IntegerField(default=1)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.TextField(max_length=500)
+    available_from = models.DateField(default=now)
+   #likers = models.ManyToManyField(User)
+
     def __str__(self):
-        return self.flat_id
-
-
-# Create your models here.
+        return self.flat_id + self.address
