6e6b4cbf427010460a44afeb636ffc5697b6cca9
Aisha <2426303b@student.gla.ac.uk>
2020-03-19 11:26:22 +0100

    added new models

--- flatshare/admin.py
+++ flatshare/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from flatshare.models import Flat, Profile
+from flatshare.models import Flat, User_addFlat, User_findFlat, Profile_addFlat, Address, Flat_images
+
 
 admin.site.register(Flat)
-admin.site.register(Profile)
+admin.site.register(Profile_addFlat)
+admin.site.register(User_addFlat)
+admin.site.register(User_findFlat)
+admin.site.register(Address)
+admin.site.register(Flat_images)
 
 # Register your models here.
--- flatshare/models.py
+++ flatshare/models.py
@@ -2,13 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class User_addFlat(models.Model):
+    MAX_CHAR = 128
+    username = models.CharField(max_length = MAX_CHAR, unique = True)
+    password_addFlat = models.CharField(max_length = MAX_CHAR, unique = False)
+    email_addflat = models.CharField(max_length = MAX_CHAR, unique = True)
+
+class User_findFlat(models.Model):
+    MAX_CHAR = 128
+    username = models.CharField(max_length = MAX_CHAR, unique = True)
+    password_findflat = models.CharField(max_length = MAX_CHAR, unique = False)
+    email_findflat = models.CharField(max_length = MAX_CHAR, unique = True)
 
 
 
-
-class Profile(models.Model):
+class Profile_addFlat(models.Model):
     NAME_MAX_CHAR = 128
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User_addFlat, on_delete=models.CASCADE)
     FirstName = models.CharField(max_length = NAME_MAX_CHAR, unique = False)
     LastName = models.CharField(max_length= NAME_MAX_CHAR, unique = False)
     picture = models.ImageField(upload_to='profile_images', blank=True)
@@ -21,13 +31,21 @@ class Flat(models.Model):
     NAME_MAX_CHAR = 128
     flat_id = models.IntegerField(default = 1, primary_key=True)
     rent = models.IntegerField(default = 1)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    street = models.CharField(max_length = NAME_MAX_CHAR, unique = False)
+    def __str__(self):
+        return self.flat_id
+
+
+class Address(models.Model):
+    MAX_CHAR = 128
+    flat = models.OneToOneField(Flat, on_delete = models.CASCADE)
+    street = models.CharField(max_length =MAX_CHAR, unique = False)
     house_no = models.CharField(max_length = 50, unique = False)
     postCode = models.CharField(max_length = 50, unique = False)
     city = models.CharField(max_length = 128, unique = False)
-    def __str__(self):
-        return self.flat_id
+
+class Flat_images(models.Model):
+    iamge_flat = models.OneToOneField(Flat, on_delete = models.CASCADE)
+    image = models.ImageField(upload_to='flat_images', blank=True)
 
 
 # Create your models here.
