bd1f6e00fdfecb5343812d228d89e86fa782f57c
Niovi Lampiri <2404353l@student.gla.ac.uk>
2020-04-01 18:36:07 +0100

    Revert "Mark - Database & Population"
    This reverts commit e2c64b53abf0b917a2178e76cc959aa85a53ea4e.

--- flatshare/admin.py
+++ flatshare/admin.py
@@ -1,24 +1,8 @@
-
 from django.contrib import admin
-from flatshare.models import Flat, UserProfile, Like, Match
+from flatshare.models import *
 
-class FlatAdmin(admin.ModelAdmin):
-    list_display = ('address', 'rent')
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('FirstName','LastName')
-
-class LikeAdmin(admin.ModelAdmin):
-    list_display = ('l_flat','l_user')
-
-class MatchAdmin(admin.ModelAdmin):
-    list_display = ('m_flat','m_user')
-
-
-
-admin.site.register(Flat,FlatAdmin)
-admin.site.register(UserProfile,UserAdmin)
-admin.site.register(Like,LikeAdmin)
-admin.site.register(Match,MatchAdmin)
+admin.site.register(Flat)
+admin.site.register(UserProfile)
+admin.site.register(Address)
 
 # Register your models here.
