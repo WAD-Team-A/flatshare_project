797732649ebd1485ecdfeef053029056600f5ebf
Aisha <2426303b@student.gla.ac.uk>
2020-04-02 14:29:22 +0100

    working on add flat form rendering

--- flatshare_project/settings.py
+++ flatshare_project/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'flatshare'
+    'flatshare',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
